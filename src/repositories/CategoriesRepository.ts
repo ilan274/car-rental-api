e49a5fb761d5efcc9fb300d3e33bf8009ccb15ca
Ilan Herbach <ilan.herbach@gmail.com>
2021-09-29 06:17:09 -0300

    Separating into modules and creating specifications

--- src/repositories/CategoriesRepository.ts
+++ src/repositories/CategoriesRepository.ts
@@ -1,4 +1,4 @@
-import { Category } from '../models/Category';
+import { Category } from '../modules/cars/model/Category';
 import {
   ICategoriesRepository,
   ICreateCategoryDTO,
--- src/repositories/ICategoriesRepository.ts
+++ src/repositories/ICategoriesRepository.ts
@@ -1,4 +1,4 @@
-import { Category } from '../models/Category';
+import { Category } from '../modules/cars/model/Category';
 
 interface ICreateCategoryDTO {
   name: string;
