3cc65692741dfc191df5ba60fa8ada7fbb259fce
Ilan Herbach <ilan.herbach@gmail.com>
2021-10-06 06:51:00 -0300

    Fix file typing

--- src/modules/cars/useCases/importCategory/ImportCategoryController.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryController.ts
@@ -4,10 +4,14 @@ import { ImportCategoryUseCase } from './ImportCategoryUseCase';
 
 class ImportCategoryController {
   constructor(private importCategoryUseCase: ImportCategoryUseCase) {}
-  handle(request: Request, response: Response) {
+  handle(request: Request, response: Response): Response {
     const { file } = request;
 
-    this.importCategoryUseCase.execute(file)
+    if (!file) {
+      return response.status(422).json({ error: 'File is required' });
+    }
+
+    this.importCategoryUseCase.execute(file);
 
     return response.status(201).json();
   }
