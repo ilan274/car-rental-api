770e0ce27f74d393a555be2ac2d603a6ff9719e5
Ilan Herbach <ilan.herbach@gmail.com>
2022-01-25 07:34:30 -0300

    Alter user table remove username column migration

--- src/modules/accounts/useCases/createUser/CreateUserController.ts
+++ src/modules/accounts/useCases/createUser/CreateUserController.ts
@@ -6,11 +6,10 @@ import { CreateUserUseCase } from './CreateUserUseCase';
 class CreateUserController {
   async handle(request: Request, response: Response): Promise<Response> {
     const createUserUseCase = container.resolve(CreateUserUseCase);
-    const { name, username, password, email, driver_licence } = request.body;
+    const { name, password, email, driver_licence } = request.body;
 
     const createdUser = await createUserUseCase.execute({
       name,
-      username,
       password,
       email,
       driver_licence,
