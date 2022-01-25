db7247e1ca520032ffd7147173fe763a8d80208a
Ilan Herbach <ilan.herbach@gmail.com>
2022-01-24 23:10:06 -0300

    Fix: add uuid function to PostgreSQL

--- src/database/migrations/1643060576664-CreateUsers.ts
+++ src/database/migrations/1643060576664-CreateUsers.ts
@@ -2,6 +2,7 @@ import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class CreateUsers1643060576664 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";');
     await queryRunner.createTable(
       new Table({
         name: 'users',
